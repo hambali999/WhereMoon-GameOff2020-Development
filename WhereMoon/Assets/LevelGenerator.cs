6228ca549d98ac89e18c5dcbc9eaabf292baa551
hambali999 <chorongbali@gmail.com>
2020-11-15 14:44:54 +0800

    Created unity game project for GameJam2020!

---
+++ WhereMoon/Assets/LevelGenerator.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelGenerator : MonoBehaviour
+{
+    public GameObject platformPrefab;
+
+    public int numberOfPlatforms = 200;
+    public float levelWidth = 10f;
+    public float minY = .2f;
+    public float maxY = 1.5f;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        Vector3 spawnPosition = new Vector3();
+
+        for (int i = 0; i < numberOfPlatforms; i++)
+        {
+            spawnPosition.y += UnityEngine.Random.Range(minY, maxY);
+            spawnPosition.x = UnityEngine.Random.Range(-levelWidth, levelWidth);
+            Instantiate(platformPrefab, spawnPosition, Quaternion.identity); 
+        }
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ WhereMoon/Assets/PlayerController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    private Rigidbody2D rb2d;
+    private float moveInput;
+    private float speed = 5f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        moveInput = Input.GetAxis("Horizontal"); //A & D, Left & Right Keys
+        rb2d.velocity = new Vector2(moveInput * speed, rb2d.velocity.y); //keep y the same, dont change when moving left and right
+    }
+}
+    
