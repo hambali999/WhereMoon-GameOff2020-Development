e70030ab8129d24524c54ab1c99d66dfb2108250
hambali999 <chorongbali@gmail.com>
2020-11-17 23:53:43 +0800

    Complete player respawn code for alien

---
+++ WhereMoon/Assets/Scripts/KillPlayer.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KillPlayer : MonoBehaviour
+{
+    [SerializeField]Transform spawnPoint;
+
+    public void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.transform.CompareTag("Player"))
+            collision.transform.position = spawnPoint.position;  //will transform the postition of the box2d collision area to back to the spawnPoint postition .
+    }
+}
