e983cebe4e2c170b4827325426dd6e5cc414c511
hambali999 <chorongbali@gmail.com>
2020-11-22 02:35:20 +0800

    solved some issues with the Blade Script

---
+++ WhereMoon/Assets/Scripts/BladeScript.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BladeScript : MonoBehaviour
+{
+    public float rotationSpeed;   //this is to make it access publicly from Unity Engine to make changes to the rotation speed!
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        this.transform.Rotate(new Vector3(0, 0, rotationSpeed));
+    }
+
+    //private void OnTriggerEnter2D(Collider2D collision)
+ //   {
+       // Destroy(collision.gameObject);
+   // }
+}
