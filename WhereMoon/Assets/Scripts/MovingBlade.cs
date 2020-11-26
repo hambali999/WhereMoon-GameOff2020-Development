a6c70c448aa1cb822076092577eccd415fc0aa8a
hambali999 <chorongbali@gmail.com>
2020-11-27 00:37:46 +0800

    added moving blade

---
+++ WhereMoon/Assets/Scripts/MovingBlade.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingBlade : MonoBehaviour
+{
+
+    public float speed;
+    public Transform pos1;
+    public Transform pos2;
+    bool turnback;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(transform.position.x >= pos1.position.x)
+        {
+            turnback = true;
+        }
+        if(transform.position.x  <= pos2.position.x)
+        {
+            turnback = false;
+        }
+        if (turnback)
+        {
+            transform.position = Vector2.MoveTowards(transform.position, pos2.position, speed*Time.deltaTime);
+        }
+        else
+        {
+            transform.position = Vector2.MoveTowards(transform.position, pos1.position, speed * Time.deltaTime);
+
+        }
+    }
+}
